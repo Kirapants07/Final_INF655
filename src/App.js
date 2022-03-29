37a18d57e66d42f2addbc2634dc586b4e406e58d
Kira Luethe <ksluethe@mail.fhsu.edu>
2022-03-29 09:58:06 -0700

    Added Users page. All caught up to end of Tutorial Week 7

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Users from "./components/User/Users";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/users/:id/:name" element={<Users />} />
       </Routes>
     </BrowserRouter>
   );
